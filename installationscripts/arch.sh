b87f0c885a17e3a2867170b3cd9eb64a61a7030f
Heisenburgh <heisenburgh@skiff.com>
2022-12-03 01:20:30 +0000

    Arch installation script

--- installationscripts/arch.sh
+++ installationscripts/arch.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 echo "Welcome to Pixarch Installation"
-echo "Do you want to install Pixarch[Y/N]:"
+read -p "Do you want to install Pixarch[Y/N]:" choice
 
-sudo pacman -S feh lightdm polybar rofi alacritty picom vim git
-echo "Done installing basic packages"
+var="Y"
+echo "$var $choice"
+if [ $choice = $var ]
+then
+    echo "Beginning installation for Pixarch..."
+    echo "Downloading packages for Pixarch..."
+    # sudo pacman -S feh lightdm polybar rofi alacritty picom vim git
+
+    echo "Installing LightDM..."
+    read -p "Do you want to install the LightDM configuration?\n\nNOTE:\n1. Enter 'b' if you want to make a backup of your config file and store it in ~/.backupconfigs/lightdm/lightdm.conf, your original file lightdm.conf will be delete and the pixarch configuration file will be installed.\n2. Enter 'y' if you want to install lightdm.conf without making a backup. This will overwrite your current config.\n3. Enter 's' to skip lightdm's installation." selection
+
+else
+    echo "Bye."
+
+fi
 
 echo "Copying files to .config directory"
 # Main config installation
