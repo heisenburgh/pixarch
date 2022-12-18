01d35326f6ec4b87a0fe30cfba856ba161a903f0
Haardispro <haardmajmudar2827@gmail.com>
2022-12-18 17:45:10 +0530

    added qtile, adding gaps and window border color change left

--- installationscripts/config.sh
+++ installationscripts/config.sh
@@ -3,4 +3,9 @@
 
 
 git clone https://github.com/heisenburgh/pixarch
-
+cd .config/
+sudo rm i3/ rofi/ alacritty/ polybar/ 
+cd ~/pixarch/config
+cp i3/ .config/
+cp rofi/ .config/
+cp polybar/ .config/
