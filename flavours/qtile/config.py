f6ed4951244459eb006e85cfc93d61fe9d3aa5d6
Haardispro <haardmajmudar2827@gmail.com>
2022-12-19 18:24:47 +0530

    added picom and polybar to qtile, stuff mostly done except rofi config

--- flavours/qtile/config.py
+++ flavours/qtile/config.py
@@ -101,15 +101,15 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    layout.Max(),
+    #layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    # layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
     # layout.MonadTall(),
     # layout.MonadWide(),
-    # layout.RatioTile(),
+    layout.RatioTile(margin=5, border_focus="#000000"),
     # layout.Tile(),
     # layout.TreeTab(),
     # layout.VerticalTile(),
@@ -125,7 +125,7 @@ extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        wallpaper='~/pixarch/wallpapers/japanesecastle.jpg',
+        wallpaper='~/pixarch/media/wallpapers/default.png',
         wallpaper_mode='fill',
         #bottom=bar.Bar(
         #    [
