90fb83b46f75261c2912e48ce42108db4434f553
Haardispro <haardmajmudar2827@gmail.com>
2024-08-23 15:43:36 +0530

    added rofi border color, starship testing file

---
+++ installation_scripts/starship.sh
@@ -0,0 +1,7 @@
+# Will merge this later into the main installation file. 
+# This is purely for testing purposes 
+
+curl -sS https://starship.rs/install.sh | sh
+cp ~/pixarch/config/starship.toml ~/.config/
+
+
